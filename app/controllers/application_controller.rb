b927645f94543c48480aded4a6e6fe104fd7efe3
Kozo Komiya <kozo.komiya@gmail.com>
2018-04-18 07:00:56 +0000

    update hello message

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   
   def hello
-    render html: "hello, world!!¡™£¢∞§¶•ªº–ˆ¨∆˙©˜∫µå∫ç∂´ƒ©˙∆ˆ∆˚¬≤µ"
+    render html: "hello, world!! 今年は2週目です。よろしくお願いします！"
   end
 end
